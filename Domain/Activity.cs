3dae1d28b148c642c1d70f2ead3e9a33cc8eb6bd
adrianojec <adrianojeric.28@gmail.com>
2023-02-16 22:41:07 +0800

    changes in Account Entity

--- Domain/Activity.cs
+++ Domain/Activity.cs
@@ -3,6 +3,8 @@ namespace Domain
     public class Activity
     {
         public Guid Id { get; set; }
+        public Guid AccountId { get; set; }
+        public Account Account { get; set; } = default!;
         public Guid TransactionId { get; set; }
         public Transaction Transaction { get; set; } = default!;
         public DateTime DateCreated { get; set; }
